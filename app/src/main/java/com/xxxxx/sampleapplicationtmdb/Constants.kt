6d01331cbb23924ff15417bf83d767d68d511109
zafer ertas <zafer.ertas1@gmail.com>
2022-06-03 19:00:57 +0300

    Update gradle

--- app/src/main/java/com/xxxxx/sampleapplicationtmdb/Constants.kt
+++ app/src/main/java/com/xxxxx/sampleapplicationtmdb/Constants.kt
@@ -1,6 +1,6 @@
 package com.xxxxx.sampleapplicationtmdb
 
-const val API_KEY: String = BuildConfig.API_KEY
+const val API_KEY: String = BuildConfig.API_KEY  
 const val API_BASE_URL = "https://api.themoviedb.org/3/"
 const val API_IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
 const val MOVIE_ID: String = "movie_id"
